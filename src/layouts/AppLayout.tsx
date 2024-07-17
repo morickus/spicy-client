7a4b74e176d5c4cc428efeb202fba032642171ce
mark <enievinas@gmail.com>
2024-07-17 14:29:34 +0300

    fix GoogleTagManager

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,3 +1,4 @@
+import { GoogleTagManager } from '@next/third-parties/google';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { FC, PropsWithChildren } from 'react';
 
@@ -7,9 +8,9 @@ const AppLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <div id="app-layout">{children}</div>
-      {/*{process.env.NODE_ENV == 'production' && (*/}
-      {/*  <GoogleTagManager gtmId="GTM-K6JB9R7H" />*/}
-      {/*)}*/}
+      {process.env.NODE_ENV == 'production' && (
+        <GoogleTagManager gtmId="GTM-K6JB9R7H" />
+      )}
     </QueryClientProvider>
   );
 };
